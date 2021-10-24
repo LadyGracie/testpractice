39627044121b41c163d7f9de096ef088d25d9f06
LadyGracie <92986332+LadyGracie@users.noreply.github.com>
2021-10-24 10:46:02 +0100

    Add files via upload
    This is another python file that was uploaded from the computer

---
+++ Hello.py
@@ -0,0 +1,10 @@
+#This is a comment 
+#Printing out hello World
+print ("Hello World")
+
+print ("python is installed") 
+
+message = 'Hello World'
+print (message.lower())
+print (message.upper())
+print (message.swapcase())
