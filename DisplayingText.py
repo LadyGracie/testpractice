604ffae56f292978f70e470cf9b0e51bc653a34a
LadyGracie <92986332+LadyGracie@users.noreply.github.com>
2021-10-24 11:04:47 +0100

    Add files via upload

---
+++ DisplayingText.py
@@ -0,0 +1,8 @@
+print ('My name is Grace\nI am a girl')
+print ('My name is Peter\tI am a boy')
+print ("""My name is John!
+       I am a boy""")
+
+
+name = input ('What is your name?\n')
+print ('Hello %s' %name)
